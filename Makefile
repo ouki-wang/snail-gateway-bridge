815f1afcaf629140a97882e03da633f2e8224d9e
ouki.wang <ouki.wang@dfrobot.com>
2022-07-14 18:31:18 +0800

    first commit

---
+++ Makefile
@@ -0,0 +1,30 @@
+.PHONY: build clean package serve run-compose-test
+VERSION := v1.0.0
+
+build:
+	@echo "Compiling source"
+	@rm -rf build
+	@mkdir build
+	go build -ldflags "-s -w -X main.version=v1.0.0" -o build/snail-gateway-bridge.exe cmd/snail-gateway-bridge/main.go
+
+clean:
+	@echo "Cleaning up workspace"
+	@rm -rf build
+	@rm -rf dist
+
+
+snapshot:
+	@goreleaser --snapshot
+
+dev-requirements:
+	go install golang.org/x/lint/golint
+	go install github.com/goreleaser/goreleaser
+	go install github.com/goreleaser/nfpm
+
+# shortcuts for development
+
+serve: build
+	./build/chirpstack-gateway-bridge
+
+run-compose-test:
+	docker-compose run --rm chirpstack-gateway-bridge make test
---
+++ cmd/snail-gateway-bridge/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"time"
+
+	"./cmd/snail-gateway-bridge/cmd"
+
+	paho "github.com/eclipse/paho.mqtt.golang"
+	log "github.com/sirupsen/logrus"
+)
+
+type pahoLogWrapper struct {
+	ln func(...interface{})
+	f  func(string, ...interface{})
+}
+
+func (d pahoLogWrapper) Println(v ...interface{}) {
+	d.ln(v...)
+}
+
+func (d pahoLogWrapper) Printf(format string, v ...interface{}) {
+	d.f(format, v...)
+}
+
+func enableClientLogging() {
+	l := log.WithField("module", "mqtt")
+	paho.DEBUG = pahoLogWrapper{l.Debugln, l.Debugf}
+	paho.ERROR = pahoLogWrapper{l.Errorln, l.Errorf}
+	paho.WARN = pahoLogWrapper{l.Warningln, l.Warningf}
+	paho.CRITICAL = pahoLogWrapper{l.Errorln, l.Errorf}
+}
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{
+		TimestampFormat: time.RFC3339Nano,
+	})
+	enableClientLogging()
+}
+
+func main() {
+	log.WithField("name", "wanghui").Info("start.....")
+	cmd.Execute("v0.0.1")
+}
